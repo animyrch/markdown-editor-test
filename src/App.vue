539885b84a86e36296f075099e29241ae5135888
e-ozcelik <e.ozcelik@lodecom.fr>
2020-12-31 12:40:40 +0100

    Filled in general layout elements in preparation.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,36 @@
 <template>
   <v-app>
+    <v-app-bar
+      app
+      flat
+      absolute
+      color="white"
+    >
+      <v-toolbar-title>Content</v-toolbar-title>
+    </v-app-bar>
     <v-main>
+      <v-card
+        outlined
+      >
+          <div>Image</div>
+          <div>Video</div>
+          <div>Description</div>
+          <div>Quote</div>
+          <div>Footnote</div>
+          <div>Link</div>
+          <div>Button</div>
+          <div>File</div>
+          <div>2 columns</div>
+          <div>Table</div>
+          <div>Table row</div>
+        <v-divider>
+        </v-divider>
+        <v-textarea
+          name="markdown-input-area"
+          value="# Header"
+          hint="**Bold** *Italic* _Underline_ List with bullets # H1 ## H2 ### H3 #### H4 ##### H5 ###### H6"
+        ></v-textarea>
+      </v-card>
     </v-main>
   </v-app>
 </template>
